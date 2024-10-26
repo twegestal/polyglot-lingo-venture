c5414edd234a49919c3ecc4e7da1186a7d655d55
twegestal <96296150+twegestal@users.noreply.github.com>
2024-10-26 22:32:35 +0200

    Added simple server test

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -4,6 +4,9 @@ export const createServer = () => {
   const app = express();
 
   app.use(express.json());
+  app.get('/hello', async (_req, res, _next) => {
+    res.status(200).json('Hello, World');
+  });
 
   return app;
 };
---
+++ apps/server/src/tests/server.test.ts
@@ -0,0 +1,8 @@
+import { it, describe, expect } from 'vitest';
+import supertest from 'supertest';
+import { createServer } from '../server';
+
+describe('Server endpoint test', () => {
+  it('Should return 200 hello world', async () =>
+    await supertest(createServer()).get('/hello').expect(200));
+});
